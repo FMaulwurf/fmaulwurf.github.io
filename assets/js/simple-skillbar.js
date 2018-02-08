8fbc60061eb74457f15ff354fdc943ee7f7e03c7
Felix Maulwurf <FMaulwurf@users.noreply.github.com>
2018-02-08 21:06:33 +0100

    Update simple-skillbar.js

--- assets/js/simple-skillbar.js
+++ assets/js/simple-skillbar.js
@@ -46,13 +46,15 @@
                 position: 'relative',
                 width: '100%',
                 backgroundColor: '#dddddd',
-                height: this.o.height+'px'
+                height: this.o.height+'px',
+                borderRadius: '5px'
             });
             i.find('.sb_bar').css({
                 position: 'absolute',
                 width: '1%',
                 height: '100%',
-                backgroundColor: this.o.background
+                backgroundColor: this.o.background,
+                borderRadius: '5px'
             });
             i.find('.sb_label').css({
                 paddingLeft: '5px',
